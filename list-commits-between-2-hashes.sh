71ee9cff3f0f0bf9a774060aa2baddb0bfaf2c2f
Huy Giang <gnhuy91@users.noreply.github.com>
2016-06-10 17:03:08 +0700

    Update list-commits-between-2-hashes.sh

--- list-commits-between-2-hashes.sh
+++ list-commits-between-2-hashes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git log \
-    --pretty=format:"\`%h\` %s - %an, %ar" \
+    --pretty=format:"\`%h\` %s - %an - %ar" \
     --since="$(git show -s --format=%ci $(cat GIT_COMMIT_CURRENT))" \
     --until="$(git show -s --format=%ci $(cat GIT_COMMIT_NEW))" \
     | head -n -1 > GIT_CHANGELOG
